dd1f7f7f926c2b30af50029ef32a9f045e2845df
huangwenguang <huangwenguang@iboxpay.com>
2020-08-14 17:55:57 +0800

    update

--- blog-admin/src/components/Home.vue
+++ blog-admin/src/components/Home.vue
@@ -1,35 +1,24 @@
 <template>
     <el-container>
         <el-aside>
-            <h5 class="admin" :style="isCollapse ? 'text-align: center' : 'text-align: left;margin-left: 20px'">
-                <span v-if="isCollapse">
-                    <i class="fa fa-user fa-lg" aria-hidden="true" style="margin-right:5px;"></i>
-                </span>
-                <span v-else>
-                    <i class="fa fa-user" aria-hidden="true" style="margin-right:5px;"></i>
-                    您好!   <span style="color:#20a0ff;font-size:20px">{{ user.username }}</span>
-                </span>
-            </h5>
             <el-menu class="el-menu-vertical" default-active="/main" unique-opened router background-color="#2C3F53"
                      text-color="#F7F7F7" :collapse="isCollapse">
+                <el-menu-item index="/">
+                    <i class="el-icon-s-custom"></i>
+                    <span slot="title">您好<span style="color:#20a0ff;font-size:20px">{{ user.username }}</span></span>
+                </el-menu-item>
                 <template v-for="(item, index) in $router.options.routes" v-if="!item.hidden">
                     <el-submenu :index="index+''" v-if="!item.leaf && item.children.length > 0">
-                        <template slot="title">
-                            <i :class="item.iconCls"></i>
-                            <span slot="title">{{ item.name }}</span>
-                        </template>
+                        <i :class="item.iconCls"></i>
+                        <span slot="title">{{ item.name }}</span>
                         <el-menu-item v-for="child in item.children" :index="child.path" v-if="!child.hidden">
-                            <template slot="title">
-                                <i :class="child.iconCls"></i>
-                                <span slot="title">{{ child.name }}</span>
-                            </template>
+                            <i :class="child.iconCls"></i>
+                            <span slot="title">{{ child.name }}</span>
                         </el-menu-item>
                     </el-submenu>
                     <el-menu-item v-if="item.leaf && !item.hidden" :index="item.children[0].path">
-                        <template slot="title">
-                            <i :class="item.children[0].iconCls"></i>
-                            <span slot="title">{{ item.children[0].name }}</span>
-                        </template>
+                        <i :class="item.children[0].iconCls"></i>
+                        <span slot="title">{{ item.children[0].name }}</span>
                     </el-menu-item>
                 </template>
             </el-menu>
@@ -54,11 +43,9 @@
                 </el-col>
 
                 <el-col :span="3" style="text-align: right;">
-                    <!--<el-badge is-dot hidden="true" style="vertical-align: top;">-->
-                        <el-tooltip class="tip" effect="dark" content="消息通知" placement="bottom">
-                            <i class="fa fa-envelope-o" aria-hidden="true" @click="userInfo"></i>
-                        </el-tooltip>
-                    <!--</el-badge>-->
+                    <el-tooltip class="tip" effect="dark" content="消息通知" placement="bottom">
+                        <i class="fa fa-envelope-o" aria-hidden="true" @click="userInfo"></i>
+                    </el-tooltip>
 
                     <el-dropdown>
                         <span class="el-dropdown-link tip">
